bd78e9cf59506b2eac6d3999091a388c1fb4962c
George Moore <mooretgeorge@gmail.com>
2021-01-19 12:28:03 +0000

    Added upload button

---
+++ frontend/src/components/Upload.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="upload">
+        <input type="file" @change="onSelected">
+        <button @click="onUpload">Upload</button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Upload",
+
+  data: () => ({
+      selectedFile: null,
+  }),
+  methods: {
+      onSelected(e: any){
+          this.selectedFile = e.target.files[0]
+      },
+      onUpload() {
+          //Send to backend
+          console.log("Uploaded:" + this.selectedFile)
+      }
+  }
+});
+</script>
+
+<style>
+</style>
