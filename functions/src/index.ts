1a7f22ee030203009ca344580fbfa285ea2f43c2
L. W. Miller <l.w.miller@lancaster.ac.uk>
2021-01-27 12:14:19 +0000

    feat(frontend): firebase wrapper class

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,8 @@ import * as functions from "firebase-functions";
 
 const region = "europe-west2";
 
-export const ping = functions.region(region).https.onRequest((req, res) => {
-  functions.logger.info("recieved ping", req, { structuredData: true });
-  res.send("Pong!");
+export const ping = functions.region(region).https.onCall((data: unknown) => {
+  functions.logger.info("recieved ping", { data });
+
+  return { data };
 });
