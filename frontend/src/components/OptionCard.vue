2eb7e8c7748a5df8d2eca55512e261ab21b8bb38
George Moore <mooretgeorge@gmail.com>
2021-02-03 11:15:42 +0000

    home

--- frontend/src/components/OptionCard.vue
+++ frontend/src/components/OptionCard.vue
@@ -4,15 +4,15 @@
       Feedback Options
     </v-card-title>
     <v-card-text>
-      <v-checkbox label="Colour contrast"></v-checkbox>
-      <v-checkbox label="Colourblindness"></v-checkbox>
+      <!-- <v-checkbox label="Colour contrast"></v-checkbox> -->
+      <!-- <v-checkbox label="Colourblindness"></v-checkbox> -->
       <v-checkbox label="Readability"></v-checkbox>
       <v-checkbox label="Text proportion"></v-checkbox>
       <v-checkbox label="Sentence length"></v-checkbox>
       <v-checkbox label="Word complexity"></v-checkbox>
       <v-checkbox label="Font type"></v-checkbox>
       <v-checkbox label="Font size"></v-checkbox>
-      <!-- <v-checkbox v-for"prop in arr" v-model="prop.state">{{ prop.name }}</v-checkbox> -->
+      <v-checkbox v-for="(prop, idx) in arr" :key="idx" v-model="prop.state" :label="arr.name"></v-checkbox>
       <h2>Do you have alt-text/an image description?</h2>
       <v-radio-group column>
         <v-radio label="Yes"></v-radio>
@@ -32,6 +32,12 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "OptionCard",
+
+  data: {
+    arr: [
+      { name:"Colour contrast", state: false },
+      { name: "Colourblindness", state: false }]
+  }
 
 });
 </script>
