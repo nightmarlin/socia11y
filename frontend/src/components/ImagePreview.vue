d73a2ea946b0db08b25eb2ad641f8a23c676c3fd
George Moore <mooretgeorge@gmail.com>
2021-02-11 13:09:13 +0000

    Updated ImagePreview component to use v-img

--- frontend/src/components/ImagePreview.vue
+++ frontend/src/components/ImagePreview.vue
@@ -4,7 +4,7 @@
             Your Image
         </v-card-title>
         <v-card-text>
-            <img src="https://play-lh.googleusercontent.com/proxy/kvrZaEmVWf_fyBffc6WB0ldHuLsFLlCMP0jsym4d_pYBmUceLQubsz606-TRApV1m5gNH7LUd8PaNUWu2TcmwpYtkKGR15qLdvdtQKQhSFEk5Xr2=w720-h405-rw" alt="Cat in the hat">
+            <v-img src="@/images/socia11y_white.png" alt="Socia11y logo"></v-img>
         </v-card-text>
     </v-card>
 </template>
