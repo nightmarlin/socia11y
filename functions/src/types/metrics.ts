1f3376cb1391005532b581d9abf63b2b84380319
L. W. Miller <l.w.miller@lancaster.ac.uk>
2021-03-02 17:30:44 +0000

    Initial structure of backend

---
+++ functions/src/types/metrics.ts
@@ -0,0 +1,39 @@
+export const textMetricNames = [
+  "overallReadability",
+  "quantity",
+  "complexity",
+  "sentenceLength",
+] as const;
+export type TextMetricTypes = typeof textMetricNames[number];
+export const imageMetricNames = [
+  "contrast",
+  "colourBlindness",
+  "hasDescription",
+  "textQuantity",
+] as const;
+export type ImageMetricTypes = typeof imageMetricNames[number];
+
+export type BaseMetric = {
+  name: string;
+  score: number;
+  explanation: string;
+};
+
+export type TextMetric = {
+  [T in TextMetricTypes]?: BaseMetric & {
+    extractedFromImage: boolean;
+  };
+};
+
+export type ImageMetric = {
+  [T in ImageMetricTypes]?: BaseMetric & {
+    focusRegion: {
+      x: number;
+      y: number;
+      height: number;
+      width: number;
+    };
+  };
+};
+
+export type Metrics = TextMetric & ImageMetric;
