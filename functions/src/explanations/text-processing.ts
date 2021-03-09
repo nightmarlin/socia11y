72225c07bf90c12511f98a4951d5b7ba2efd05bd
Luke <l.halpin@lancaster.ac.uk>
2021-03-09 13:54:22 +0000

    pointless
    I wanted to check something

--- functions/src/explanations/text-processing.ts
+++ functions/src/explanations/text-processing.ts
@@ -7,7 +7,7 @@ const sentenceLength = {
     "If you have sentences that are too long it can lead to the sentence becoming complicated and difficult to follow. This can also lead to the reader losing interets in the content and possibly missing important information.",
   manyShort:
     "Having sentences that are too short can make your text feel clunky and poorly structured. Having some short sentences for effect can be fine, but too many starts to make the flow of the text difficult to follow. This can lead to the overall text becoming confusing and difficult to understand.",
-  // mixed: "",
+  // mixed: "", //unsure what to do with
 } as const;
 
 export const explanations = {
